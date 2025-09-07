8b098459b3fa8847583a0a24a1382c67532ea588
sonalrmeshram <sonalrmeshram@gmail.com>
2025-09-07 06:51:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "master" {
+  name = "veera-eks-master1"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
